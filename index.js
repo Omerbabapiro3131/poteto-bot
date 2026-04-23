f883bea06a1c5d123bd2b0cd681bdba258307d91
Omerbabapiro3131 <omer02072016@gmail.com>
2026-04-23 20:37:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const mineflayer = require('mineflayer')
 const http = require('http')
 
-// Replit'in botu "bozuk" sanıp resetlemesini engellemek için sahte server
+// Render'ın "çalışıyor" görmesi için basit bir web sunucusu
 http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end("POTETO24_ONLINE");
+    res.write("POTETO24 AKTIF");
+    res.end();
 }).listen(process.env.PORT || 3000);
 
 function createBot() {
@@ -13,23 +13,19 @@ function createBot() {
         port: 58682,
         username: 'POTETO24',
         version: '1.21.1',
-        // Sunucu lag yaparsa hemen düşmemesi için limitleri kökle
         connectTimeout: 120000,
         checkTimeoutInterval: 120000
     })
 
-    bot.on('login', () => console.log('POTETO24 içeri daldı ve sabitlendi.'));
+    bot.on('login', () => console.log('Bot sunucuya çakıldı.'));
 
     bot.on('end', () => {
-        // Atıldığı an (0 saniye bekleme) geri girmeye çalışır
+        // Atılırsa hemen (saniyesinde) geri gir
         createBot();
     });
 
-    // Hata verse bile kodu durdurma, devam et
     bot.on('error', (err) => {
-        console.log('Bağlantı hatası, tekrar deneniyor...');
-        setTimeout(createBot, 1000);
+        setTimeout(createBot, 2000);
     });
 }
-
 createBot()
