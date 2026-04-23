e28d091bf9f8cc46f368b81881997ed212b0180a
Omerbabapiro3131 <omer02072016@gmail.com>
2026-04-23 20:20:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+const mineflayer = require('mineflayer')
+const http = require('http')
 
+// Replit'in projeyi kapatmaması için şart
+http.createServer((req, res) => {
+    res.end("POTETO24 Arka Planda Aktif");
+}).listen(process.env.PORT || 3000);
+
+function createBot() {
+    const bot = mineflayer.createBot({
+        host: 'potetosmp11.aternos.me',
+        port: 58682,
+        username: 'POTETO24',
+        version: '1.21.1',
+        // Sunucuyla olan bağlantı kontrolünü gevşetelim ki durduk yere atmasın
+        checkTimeoutInterval: 120000 
+    })
+
+    bot.on('login', () => console.log('POTETO24 sessizce girdi.'))
+
+    bot.on('end', (reason) => {
+        console.log('Bağlantı koptu, 10 saniye sonra tekrar deniyor...');
+        setTimeout(createBot, 10000)
+    })
+
+    bot.on('error', (err) => console.log('Hata oluştu:', err))
+}
+
+createBot()
